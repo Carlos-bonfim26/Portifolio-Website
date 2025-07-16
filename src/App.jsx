2f99ca671d20e5ad165d4825758d7d58b915b167
carlosbonfim <carlosbonfim722@gmail.com>
2025-07-16 17:26:21 -0300

    feat: about-me

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Home from "./pages/Home";
 import "./App.css";
+import AboutMe from "./pages/AboutMe";
 
 function App() {
   return(
   <>
-  <Home></Home>
+  <Home/>
+  <AboutMe/>
   </>
   );
 }
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import img from '../img/carlosbonfim.png'
+import './AboutMe.css'
 const AboutMe = () => {
   return (
-    <div>
-      
+    <div className='container-about'>
+      <div className="img-about">
+        
+        <img src={img} alt="Carlos Bonfim" />
+        <div className="border-rectangle"></div>
+         <div className="rectangle"></div>
+      </div>
+      <div className="text-about">
+        <div className="border-rectangle"></div>
+        <h2>Quem <span>Sou?</span></h2>
+        <p>Olá, me chamo Carlos Bonfim, tenho 19 anos, estou cursando o Análise e Desenvolvimento de Sistemas na Universidade Anhembi Morumbi, tenho certificados de Suporte em T.I e Desenvolvimento Web no Instituto da Oportunidade Social, tendo participado uma vez do evento de melhores TCC’s que acontece na sede da TOTVS que é patrocinadora do Curso, também tenho certificado Proa e Senac no curso Proa Profissão onde se aprende desenvolvimento Full-Stack e onde eu desenvolvi o projeto Saboria abordado mais embaixo no site, sou uma pessoa comunicativa e analítica, tendo o esforço como uma das minhas principais virtudes, e se você quer saber mais um pouco sobre mim, explore o portfólio e veja mais um pouco do que já fiz.</p>
+        <div className="rectangle"></div>
+      </div>
     </div>
   )
 }
