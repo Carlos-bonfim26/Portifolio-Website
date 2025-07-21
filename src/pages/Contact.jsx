b5cd5ef81a50c18bdae0960260002d2ba31af78c
carlosbonfim <carlosbonfim722@gmail.com>
2025-07-21 20:07:17 -0300

    feat: button download CV

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { forwardRef } from 'react'
 import contatoImg from '../img/contact.svg'
+import pdfCv from '../img/2025.07_curriculo_carlosbonfim.pdf'
 const Contact = forwardRef((props, ref) => {
   return (
     <div className='contact' ref={ref}>
@@ -9,6 +10,7 @@ const Contact = forwardRef((props, ref) => {
           <a href="https://www.linkedin.com/in/carlosbonfim26/" target='_blank'>Linkedin</a>
           <a href="mailto:Carlosbonfim772@gmail.com" >carlosbonfim772@gmail.com</a>
           <a href="https://www.instagram.com/carlosbonfim_dw/" target='_blank'>Instagram</a>
+          <a download={pdfCv} href={pdfCv}  className="button-cv"><button>Baixar CV</button></a>
         </div>
         <img src={contatoImg} alt="" />
       </div>
